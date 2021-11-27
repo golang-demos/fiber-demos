cec0dfbfd322238bcf086bf01a89b2cd54b527a6
Vinay Jeurkar <vjeurkar03@gmail.com>
2021-11-27 11:48:03 +0530

    Added get route demo with different types of parameters

---
+++ 02-get-with-params/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func greetHandler(c *fiber.Ctx) error {
+	name := c.Params("name")
+	c.SendString("Hello " + name + "!")
+	return nil
+}
+
+func multipleParamsHandler(c *fiber.Ctx) error {
+	postId := c.Params("postId")
+	commentId := c.Params("commentId")
+	c.SendString("Showing Comment with id : " + commentId + " for Post with Id : " + postId)
+	return nil
+}
+
+func wildCardHandler(c *fiber.Ctx) error {
+	anyParam := c.Params("*")
+	c.SendString("Passed string : " + anyParam)
+	return nil
+}
+
+func main() {
+	app := fiber.New()
+
+	// Open : http://localhost:3000/greet/Vinay
+	app.Get("/greet/:name", greetHandler)
+
+	// Open : http://localhost:3000/posts/12/comments/415
+	app.Get("/posts/:postId/comments/:commentId", multipleParamsHandler)
+
+	// Open : http://localhost:3000/wildcard/any/parameter/type
+	app.Get("/wildcard/*", wildCardHandler)
+
+	err := app.Listen(":3000")
+	if err != nil {
+		fmt.Println("[ERROR] Failed to start server")
+	}
+}
