19351b288928a42e3cb82e05a5006e7cd4cb3ddb
Vinay Jeurkar <vjeurkar03@gmail.com>
2021-11-27 21:37:05 +0530

    Updated static files demo

--- 03-static-files/main.go
+++ 03-static-files/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -14,7 +15,14 @@ func main() {
 	pwd, _ := os.Getwd()
 	rootPath := pwd + "/public"
 	fmt.Println("Root Path : ", rootPath)
-	app.Static("/", rootPath)
+	app.Static("/", rootPath, fiber.Static{
+		Compress:      true,
+		ByteRange:     true,
+		Browse:        true,
+		Index:         "index.html",
+		CacheDuration: 10 * time.Second,
+		MaxAge:        3600,
+	})
 
 	err := app.Listen(":3000")
 	if err != nil {
