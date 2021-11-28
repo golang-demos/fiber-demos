f26d675b07c3cf79b8bc2f0b24881deb2866e30e
Vinay Jeurkar <vjeurkar03@gmail.com>
2021-11-28 10:54:24 +0530

    Added routes grouping demo

---
+++ 08-routes-grouping-demo/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func apiGroupHandler(c *fiber.Ctx) error {
+	accessToken := c.Cookies("Access-Token")
+	if accessToken != "" { // Do not allow if Access Token is not set in cookie
+		return c.Next()
+	} else {
+		log.Println("Access token is missing")
+	}
+	return nil
+}
+
+func postsGroupHandler(c *fiber.Ctx) error {
+	fmt.Println("In Posts groups Handler")
+	return c.Next() // This statement is mandatory to execute other routes registered in this group.
+}
+
+func postDetailsHandler(c *fiber.Ctx) error {
+	fmt.Println("In Posts Details Handler")
+	return nil
+}
+func postDeleteHandler(c *fiber.Ctx) error {
+	fmt.Println("In Posts delete Handler")
+	return nil
+}
+
+func main() {
+	app := fiber.New()
+
+	api := app.Group("/api", apiGroupHandler)
+
+	postsApi := api.Group("/posts", postsGroupHandler)
+
+	postsApi.Get("/:id", postDetailsHandler)
+	postsApi.Delete("/:id", postDeleteHandler)
+
+	err := app.Listen(":3000")
+	if err != nil {
+		fmt.Println("Failed to start server")
+	}
+}
