1570e75a392142c03372ab779471d14ad7fe2e5f
Vinay Jeurkar <vjeurkar03@gmail.com>
2021-12-01 23:56:55 +0530

    Updatd request details demo

--- 13-request-details/main.go
+++ 13-request-details/main.go
@@ -9,18 +9,31 @@ import (
 func detailsHandler(c *fiber.Ctx) error {
 	data := struct {
 		Hostname          string
+		Protocol          string
 		IpAddress         string
 		ForwaredIpAddress []string
+		Subdomains        []string
+		URL               string
+		Path              string
 	}{}
 
 	// hostname for URL
 	data.Hostname = c.Hostname()
+
+	data.Protocol = c.Protocol()
 
 	// IP Address of the user visiting this route
 	data.IpAddress = c.IP()
 
 	// IP Addresses taken from X-Forwarded-For Header
 	data.ForwaredIpAddress = c.IPs()
+
+	// original request URL
+	data.URL = c.OriginalURL()
+
+	data.Path = c.Path()
+
+	data.Subdomains = c.Subdomains()
 
 	return c.JSON(data)
 }
